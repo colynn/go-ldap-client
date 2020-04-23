74ee80cb797ab9c284b62eb2881014d048de1393
colynn.liu <colynn.liu@gmail.com>
2020-04-23 10:53:54 +0800

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"ldap"
+	"log"
+)
+
+var base, bindDN, bindPassword, groupFilter, host, password, serverName, userFilter, username string
+var port int
+var useSSL bool
+var skipTLS bool
+
+type server struct{}
+
+func main() {
+	flag.Parse()
+
+	client := &ldap.Client{
+		Base:         base,
+		Host:         host,
+		Port:         port,
+		UseSSL:       useSSL,
+		SkipTLS:      skipTLS,
+		BindDN:       bindDN,
+		BindPassword: bindPassword,
+		UserFilter:   userFilter,
+		GroupFilter:  groupFilter,
+		Attributes:   []string{"givenName", "sn", "mail", "uid"},
+		ServerName:   serverName,
+	}
+	defer client.Close()
+
+	ok, user, err := client.Authenticate(username, password)
+	if err != nil {
+		log.Fatalf("Error authenticating user %s: %+v", username, err)
+	}
+	if !ok {
+		log.Fatalf("Authenticating failed for user %s", username)
+	}
+	log.Printf("User: %+v", user)
+
+	groups, err := client.GetGroupsOfUser(username)
+	if err != nil {
+		log.Fatalf("Error getting groups for user %s: %+v", username, err)
+	}
+	log.Printf("Groups: %+v", groups)
+}
+
+func init() {
+	flag.StringVar(&base, "base", "dc=example,dc=com", "Base LDAP")
+	flag.StringVar(&bindDN, "bind-dn", "ldap@example.com", "Bind DN")
+	flag.StringVar(&bindPassword, "bind-pwd", "example198..,", "Bind password")
+	flag.StringVar(&groupFilter, "group-filter", "(memberUid=%s)", "Group filter")
+	flag.StringVar(&host, "host", "ldap.example.com", "LDAP host")
+	flag.StringVar(&password, "password", "password", "Password")
+	flag.IntVar(&port, "port", 389, "LDAP port")
+	flag.StringVar(&userFilter, "user-filter", "(samaccountname=%s)", "User filter")
+	flag.StringVar(&username, "username", "username", "Username")
+	flag.StringVar(&serverName, "server-name", "", "Server name for SSL (if use-ssl is set)")
+	flag.BoolVar(&useSSL, "use-ssl", false, "Use SSL")
+	flag.BoolVar(&skipTLS, "skip-tls", false, "Skip TLS start")
+}
---
+++ ldap-client_test.go
@@ -0,0 +1,75 @@
+package ldap
+
+import (
+	"log"
+	"testing"
+)
+
+func TestAuthenticate(t *testing.T) {
+	client := &Client{
+		Base:               "ou=example,dc=example,dc=com",
+		Host:               "ldap.example.cn",
+		Port:               389,
+		UseSSL:             false,
+		BindDN:             "ldap@example.com",
+		BindPassword:       "bindpassword",
+		UserFilter:         "(samaccountname=%s)",
+		GroupFilter:        "(memberUid=%s)",
+		Attributes:         []string{"givenName", "sn", "mail", "uid"},
+		SkipTLS:            false,
+		ServerName:         "ldap.example.cn",
+		InsecureSkipVerify: true,
+	}
+	defer client.Close()
+
+	ok, user, err := client.Authenticate("username", "password")
+	if err != nil {
+		log.Fatalf("Error authenticating user %s: %+v", "username", err)
+	}
+	if !ok {
+		log.Fatalf("Authenticating failed for user %s", "username")
+	}
+	log.Printf("User: %+v", user)
+}
+
+func TestFindUser(t *testing.T) {
+	client := &Client{
+		Base:               "OU=example,DC=example,DC=com",
+		Host:               "ldap.example.com",
+		Port:               389,
+		UseSSL:             false,
+		BindDN:             "ldap@example.com",
+		BindPassword:       "bindpassword",
+		UserFilter:         "(uid=%s)",
+		GroupFilter:        "(memberUid=%s)",
+		Attributes:         []string{"givenName", "sn", "mail", "uid"},
+		SkipTLS:            true,
+		ServerName:         "ldap.example.com",
+		InsecureSkipVerify: true,
+	}
+	defer client.Close()
+
+	user, err := client.FindUser("username")
+	if err != nil {
+		log.Fatalf("Error get user : %s", err.Error())
+	}
+
+	log.Printf("User: %+v", user)
+}
+
+func TestGetGroupsOfUser(t *testing.T) {
+	client := &Client{
+		Base:         "dc=example,dc=com",
+		Host:         "ldap.example.cn",
+		Port:         389,
+		GroupFilter:  "(memberUid=%s)",
+		BindDN:       "ldap@example.com",
+		BindPassword: "bindpassword",
+	}
+	defer client.Close()
+	groups, err := client.GetGroupsOfUser("username")
+	if err != nil {
+		log.Fatalf("Error getting groups for user %s: %+v", "username", err)
+	}
+	log.Printf("Groups: %+v", groups)
+}
